b0d05d0c383831866b8d049b278d56c48beda605
dawei jia <jiadw007@gmail.com>
2014-03-12 23:12:44 -0400

    now word filter works

--- filter.py
+++ filter.py
@@ -15,13 +15,40 @@ class Filter(webapp2.RequestHandler):
     """Word filter class"""
 
     #default wordList
-
+    wordList = ["maverick","ranger","rocket","chicago","bear","wizard","seattle",
+                "atlanta","miami","austin","boston","pittsburgh","heat","saint",
+                "hawk","king"]
 
     def post(self):
 
         """ Post method to filtering """
+        #get user input text
         text = self.request.get('param')
-        self.response.out.write(str(text) + " Hello World!")
+        textList = str(text).split(" ")
+        result = []
+        for word in textList:
+
+            length = len(word)
+            if word in self.__class__.wordList: #if word is in wordList REPLACE WORD WITH *
+                result.append('*' * length)
+            else:   #if word is not in wordList, splice word to check whether the substring in the word is in the wordList
+                start = 1
+                while start < length:
+                    front_part = word[:start]
+                    end_part = word[start:]
+                    #if two parts in the word are in the wordList, REPLACE WORD WITH *
+                    if front_part in self.__class__.wordList and end_part in self.__class__.wordList:
+                        result.append('*' * length)
+                        break
+                    start = start + 1
+                #if all substring combination are not in the wordList, append word to result list
+                if start == length:
+                    result.append(word)
+        #construct result word List
+        word = " ".join(result)
+        self.response.out.write(word)
+
+        ##self.response.out.write(str(text) + " Hello World!")
 
 
 application = webapp2.WSGIApplication([("/filter", Filter)],debug = True)
